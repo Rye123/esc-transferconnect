3a7e4f4c0db6b7ca3d3f48d7b2c273a4f14faa26
Ryan Toh <Rye123@users.noreply.github.com>
2022-06-24 02:23:00 +0800

    proper authentication implemented on server
    refactor: JWT authentication moved to separate file,
    feat: endpoints implemented for login, session, and logout

--- bank/bank-server/index.js
+++ bank/bank-server/index.js
@@ -9,6 +9,9 @@ const express = require('express');
 const morgan = require('morgan'); // Logging middleware: https://expressjs.com/en/resources/middleware/morgan.html
 const cookieParser = require('cookie-parser'); // middleware for cookies
 
+/* Service Imports */
+const JWT_Auth = require('./services/JWT_Auth');
+
 /* Express Setup */
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -35,62 +38,82 @@ app.use(morgan('dev'));  // logs requests
 // Temporary database
 users = [
     {
-        "id": 1,
+        "id": "1",
         "username": "johndoe1",
         "password": "password",
         "points": 42
     },
     {
-        "id": 2,
+        "id": "2",
         "username": "michaelmyers123",
         "password": "halloween",
         "points": 69
     },
     {
-        "id": 3,
+        "id": "3",
         "username": "mrpotatohead",
         "password": "onetwothree",
         "points": 123
     },
     {
+        "id": "4",
         "username": "asdf",
         "password": "fdsa",
-        "id": 4
+        "points": 52
     }
 ]
 
-
-/* JWT Authentication (TODO: move to another module) */
-const jwt = require('jsonwebtoken');
-const TOKEN_EXPIRY = '1800s'; // 30 minutes
-
-const generateAuthToken = (payload) => {
-    return jwt.sign(payload, process.env.JWT_TOKEN_SECRET, { expiresIn: '1800s' });
-}
-
-const authenticateAuthToken = (request, response, next) => {
-    // sets request.user to the auth token's user if it is verified
-    // const authHeader = request.headers['authorization'];
-    // const authToken = authHeader && authHeader.split(' ')[1]; // gives the authentication token if it exists
-    const authToken = request.cookies['authToken'];
-
-    if (authToken == null)
-        return response.status(401).send({ error: 'authentication required' });
-    
-    jwt.verify(authToken, process.env.JWT_TOKEN_SECRET, (error, user) => {
-        if (error)
-            return response.status(403).send({ error: 'authentication failure' });
-        request.user = user;
-        next();
-    })
-}
-
-
-// DEBUG: get request for users
-app.get('/api/users', authenticateAuthToken, (request, response) => {
+// DEBUG: get request for users -- should be deleted
+app.get('/api/users', JWT_Auth.requireAuth, (request, response) => {
     response.json(users);
 });
 
+app.get('/api/users/:id', JWT_Auth.requireAuth, (request, response) => {
+    // check request.payload (from requireAuth) if user is authorised to view this
+    if (!request.payload || request.params.id !== request.payload.id) {
+        return response.status(403).json({ error: 'unauthorised' });
+    }
+
+    // otherwise, we return the user's information other than the password
+    const foundUser = users.find(user => user.id === request.params.id);
+    if (!foundUser)
+        return response.status(500).json({ error: 'internal service error' }); // somehow this user doesn't exist anymore?
+    const userInfo = {
+        id: foundUser.id,
+        username: foundUser.username,
+        points: foundUser.points
+    };
+    return response.status(200).json(userInfo);
+});
+
+app.get('/api/token-resolve', JWT_Auth.requireAuth, (request, response) => {
+    if (!request.payload) {
+        return response.status(403).json({ error: 'unauthorised' });
+    }
+    let user_id = request.payload.id;
+    const foundUser = users.find(user => user.id === user_id);
+    if (!foundUser)
+        return response.status(500).json({ error: 'internal service error' }); // somehow this user doesn't exist anymore?
+    const userInfo = {
+        id: foundUser.id,
+        username: foundUser.username,
+        points: foundUser.points
+    };
+    return response.status(200).json(userInfo);
+});
+
+// TODO: Refactor to move cookie handling to auth
+app.post('/api/token-terminate', JWT_Auth.requireAuth, (request, response) => {
+    if (!request.payload) {
+        return response.status(403).json({ error: 'unauthorised' });
+    }
+    response.clearCookie(
+        JWT_Auth.cookie_name,
+        { httpOnly: true, secure: true }
+    ).status(204).send();
+});
+
+// TODO: Refactor to move cookie handling to auth
 app.post('/api/token-auth', (request, response) => {
     if (!request.body.username || !request.body.password) {
         return response.status(400).json({ error: 'missing information' });
@@ -103,14 +126,21 @@ app.post('/api/token-auth', (request, response) => {
 
     const foundUser = users.find(user => user.username === testUser.username && user.password === testUser.password);
     if (foundUser) {
-        const token = generateAuthToken({username: foundUser.username})
-        console.log(`Setting cookie with ${token}`)
-        response.cookie("authToken", token, { httpOnly: true, secure: true }).status(201).send("Cookie Set");
-        //return response.status(201).json(token);
+        // Set a cookie on the client-side with the given cookie_name and the given token.
+        const userInfo = {
+            id: foundUser.id,
+            username: foundUser.username,
+            points: foundUser.points
+        };
+        response.cookie(
+            JWT_Auth.cookie_name,
+            JWT_Auth.generateToken({id: foundUser.id, username: foundUser.username}), 
+            { httpOnly: true, secure: true }
+        ).status(201).json(userInfo);
     } else {
         return response.status(401).json({ error: 'authentication failed' })
     }
-})
+});
 
 
 /* Unknown Endpoint Handling */
