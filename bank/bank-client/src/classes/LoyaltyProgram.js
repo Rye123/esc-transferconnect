d54245dd6ba20f95574af5c859ba6230b78cb779
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-16 01:59:08 +0800

    feat: loading of programs from array

--- bank/bank-client/src/classes/LoyaltyProgram.js
+++ bank/bank-client/src/classes/LoyaltyProgram.js
@@ -5,10 +5,22 @@
     /**
      * 
      * @param {string} loyaltyProgramId ID of the loyalty program
+     * @param {string} loyaltyProgramName 
      * @param {number} exchangeRate ratio of TC points to the loyalty program points
+     * @param {string} href 
+     * @param {string} imgSrc 
+     * @param {number} estTransferTime 
+     * @param {number} minTransfer 
      */
-    constructor (loyaltyProgramId, exchangeRate) {
+    constructor (loyaltyProgramId, loyaltyProgramName, exchangeRate, href="", imgSrc="", estTransferTime=1, minTransfer=0) {
         this.loyaltyProgramId = loyaltyProgramId;
+        this.loyaltyProgramName = loyaltyProgramName;
         this.exchangeRate = exchangeRate;
+        this.href = href;
+        this.imgSrc = imgSrc;
+        this.estTransferTime = estTransferTime;
+        this.minTransfer = minTransfer;
     }
 }
+
+export default LoyaltyProgram;
