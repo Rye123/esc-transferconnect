f03bee1c6fe89eb836b818268b637088365cff28
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-15 22:46:21 +0800

    feat: add 404 page

--- bank/bank-client/src/routes/NotFound.js
+++ bank/bank-client/src/routes/NotFound.js
@@ -1,7 +1,19 @@
+/* Styling */
+import './NotFound.css';
+
+
 const NotFound = () => {
     return (
         <main>
-            <h1>404 Not Found</h1>
+            <img class ="wave" src='images/wave.png' />
+            <div class = "container">
+                <div class = "img">
+                    <img src='images/404.svg' alt="404 error" />
+                </div>
+                <div class = "error-container">
+                    <h2>404 Error: Page not found</h2>
+                </div>
+            </div>
         </main>
     )
 }
