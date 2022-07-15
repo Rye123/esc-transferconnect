f4c730c538e964c3187173f3118990cfa463afd2
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-15 22:57:37 +0800

    (WIP) feat: transfer history page

--- bank/bank-client/src/routes/TransferHistory.js
+++ bank/bank-client/src/routes/TransferHistory.js
@@ -1,5 +1,8 @@
 import { useContext } from 'react'
 
+/* Styling */
+import './TransferHistory.css';
+
 /* Contexts */
 import userContext from '../contexts/userContext';
 
@@ -8,7 +11,41 @@ const TransferHistory = () => {
     
     return (
         <main>
-            <h1>Transfer History</h1>
+            <h1>Transfer History</h1><br />
+            <table class="content-table">
+                <thead>
+                <tr>
+                    <th>S.No</th>
+                    <th>Transfer ID</th>
+                    <th>Program</th>
+                    <th>Membership Number</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>100005</td>
+                    <td>Dynasty Flyer</td>
+                    <td>H1234567</td>
+                    <td>COMPLETED</td>
+                </tr>
+                <tr class="active-row">
+                    <td>2</td>
+                    <td>100006</td>
+                    <td>Emirates Skywards</td>
+                    <td>P1234567</td>
+                    <td>PENDING</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>100007</td>
+                    <td>Asia Miles</td>
+                    <td>G1234567</td>
+                    <td>PENDING</td>
+                </tr>
+                </tbody>
+            </table>
         </main>
     )
 }
