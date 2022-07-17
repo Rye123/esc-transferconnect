5649a85a0fbcb74f1d270701ee198b3304d0d2a6
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-17 17:40:02 +0800

    (WIP) feat: ui for loyalty program info page

--- bank/bank-client/src/routes/LoyaltyProgramInfoPage.js
+++ bank/bank-client/src/routes/LoyaltyProgramInfoPage.js
@@ -1,14 +1,51 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Utils from '../utils/utils';
+import loyaltyPrograms_service from '../services/loyaltyPrograms_service';
 
 /**
  * LoyaltyProgramInfoPage - Displays loyalty program data for a single loyalty program
  */
 const LoyaltyProgramInfoPage = () => {
-    // TODO: resolve loyaltyProgramId into loyaltyProgram
+    const [loyaltyProgram, setLoyaltyProgram] = useState({});
     const params = useParams();
     const loyaltyProgramId = params.loyaltyProgramId;
+
+    loyaltyPrograms_service.programs_getProgramById(loyaltyProgramId)
+    .then(loyaltyProgram => {
+        console.log("loaded");
+        setLoyaltyProgram(loyaltyProgram);
+    })
+    .catch(err => {
+        console.error("LoyaltyProgramInfoPage Error: ", err);
+        setLoyaltyProgram({});
+    })
+
+    
+    // Return HTML
+    if (Utils.isEmptyObject(loyaltyProgram))
+        return (<h1>Loading...</h1>);
     return (
-        <h1>Loyalty Program ID: {loyaltyProgramId}</h1>
+        <>
+            <img className="wave" src='images/wave.png' />
+            <div className='container'>
+                <div className='img'>
+                    <img src='images/info.svg' alt="Information" />
+                </div>
+                <div className='container2'>
+                    <div className='container3'>
+                        <a href={`${loyaltyProgram.href}`}>
+                            <img className='avatar' src={`${loyaltyProgram.imgSrc}`} alt=''/>
+                        </a>
+                        <h3>{loyaltyProgram.loyaltyProgramName}</h3>
+                        <p>{loyaltyProgram.description}</p>
+
+                        <button className='btn' type='submit'>Edit/Save Membership</button>
+                        <button className='btn' type='submit'>Transfer Points</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- bank/bank-client/src/services/loyaltyPrograms_service.js
+++ bank/bank-client/src/services/loyaltyPrograms_service.js
@@ -5,17 +5,19 @@
 
 /* Imports */
 import axios from 'axios';
-import { resolvePath } from 'react-router-dom';
 import Utils from '../utils/utils';
 import LoyaltyProgram from '../classes/LoyaltyProgram';
+import LoyaltyProgramMembership from '../classes/LoyaltyProgramMembership';
 
 /* Constants */
 // temporary storage
 const loyaltyPrograms = [];
 loyaltyPrograms.push(new LoyaltyProgram("2341", "Asia Miles", 400/250, "asia_miles_info.html", "images/asia-miles.jpeg", 4*7, 400));
 loyaltyPrograms.push(new LoyaltyProgram("1234", "British Airways", 400/312.5, "british_airway_info.html", "images/british-airways.jpeg", 4*8, 400));
-loyaltyPrograms.push(new LoyaltyProgram("2468", "Dynasty Flyer", 400/250, "dynasty_flyer_info.html", "images/china-airlines.jpeg", 4*4, 400))
-loyaltyPrograms.push(new LoyaltyProgram("6432", "Emirates Skywards", 400/250, "emirates_skywards_info.html", "images/emirates.png", 4*4, 400))
+loyaltyPrograms.push(new LoyaltyProgram("2468", "Dynasty Flyer", 400/250, "dynasty_flyer_info.html", "images/china-airlines.jpeg", 4*4, 400));
+loyaltyPrograms.push(new LoyaltyProgram("6432", "Emirates Skywards", 400/250, "emirates_skywards_info.html", "images/emirates.png", 4*4, 400));
+const memberships = [];
+memberships.push(new LoyaltyProgramMembership("A1234", "xxyy", "2341"));
 // future endpoints for program data
 const SERVER_URI = "/api/";
 const PROGRAMS_URI = SERVER_URI + "programs";
@@ -41,6 +43,28 @@ const programs_getProgramById = async(id) => {
         return Promise.reject({ "error": "Loyalty Program doesn't exist"});
     }
     return Promise.resolve(loyaltyProgram);
+}
+
+/**
+ * Gets a loyalty program membership for the current user, given the loyaltyProgramId
+ * @param {string} loyaltyProgramId 
+ * @returns Promise. Resolves to give the membership if successful.
+ */
+const programs_getMembershipForProgram = async(loyaltyProgramId) => {
+    // TODO: Submit credentials in cookie when sending to server, server should only return the membership if the membership belongs to the user too.
+    const loyaltyProgram = loyaltyPrograms.find(loyaltyProgram => loyaltyProgram.loyaltyProgramId === loyaltyProgramId);
+
+    if (Utils.isEmptyObject(loyaltyProgram)) {
+        return Promise.reject({ "error": "Loyalty Program doesn't exist"});
+    }
+
+    const loyaltyProgramMembership = memberships.find(membership => membership.loyaltyProgramId === loyaltyProgramId);
+
+    if (Utils.isEmptyObject(loyaltyProgramMembership)) {
+        return Promise.reject({ "error": "Membership doesn't exist"});
+    }
+
+    return Promise.resolve(loyaltyProgramMembership);
 }
 
 const exports = { programs_getAllPrograms, programs_getProgramById }
