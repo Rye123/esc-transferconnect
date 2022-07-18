c07daba6b7f85188814d54f1faf58e2254dfce63
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-18 16:06:38 +0800

    (WIP) feat: transfer page ui

--- bank/bank-client/src/routes/LoyaltyProgramMakeTransferPage.js
+++ bank/bank-client/src/routes/LoyaltyProgramMakeTransferPage.js
@@ -3,6 +3,9 @@ import { Navigate, useSearchParams, useNavigate } from 'react-router-dom';
 import Utils from '../utils/utils';
 import loyaltyPrograms_service from '../services/loyaltyPrograms_service';
 
+/* Hooks */
+import { useUserAuth } from '../hooks/UserAuthContext';
+
 /**
  * LoyaltyProgramMakeTransferPage - Form to allow user to make a single accrual transfer, for a particular program.
  */
@@ -10,6 +13,8 @@ const LoyaltyProgramMakeTransferPage = () => {
     const [loyaltyProgramMembership, setLoyaltyProgramMembership] = useState(undefined);
     const [loyaltyProgram, setLoyaltyProgram] = useState(undefined);
     const [searchParams, setSearchParams] = useSearchParams();
+    const userAuth = useUserAuth();
+    const user = userAuth.user;
 
     // Load Loyalty Program ID
     const loyaltyProgramId = searchParams.get("loyaltyProgramId") || "";
@@ -38,7 +43,7 @@ const LoyaltyProgramMakeTransferPage = () => {
     const handleFormSubmission = (event) => {
         event.preventDefault();
     }
-    
+    let currentPoints = 50;
     // Return HTML
     if (typeof loyaltyProgramMembership === 'undefined' || typeof loyaltyProgram === 'undefined')
         return (<h1>Loading...</h1>);
@@ -56,6 +61,28 @@ const LoyaltyProgramMakeTransferPage = () => {
                 <div className='form-container'>
                     <form onSubmit={handleFormSubmission}>
                         <h3>Transfer Points to {loyaltyProgram.loyaltyProgramName}</h3>
+                        <h2>{loyaltyProgramMembership.loyaltyProgramMembershipId}</h2>
+                        <div className='box'>
+                            <b>AVAILABLE: {user.points}</b><br /><br />
+                            <b>USING: {currentPoints}</b><br /><br />
+                            <b>REMAINING: {user.points - currentPoints}</b><br /><br />
+                        </div>
+                        <br />
+                        <br />
+                        <h3>Total Points to Transfer</h3>
+                        <div className='input-div two'>
+                            <div className='i'>
+                                <i className='material-icons'>card_membership</i>
+                            </div>
+                            <div>
+                                <input className='input' type='password' placeholder='Enter the number of points...' /><br />
+                            </div>
+                        </div>
+                        <br />
+                        <h3>Equates to {currentPoints * loyaltyProgram.exchangeRate} points</h3>
+                        <button className='btn' type='submit'>Complete Transfer</button>
+                        <h5>All transfers are final</h5><br />
+                        <h6>Once rewards have been transfered, they are subject to the terms of the Loyalty Program to which they are transferred</h6>
                     </form>
                 </div>
             </div>
