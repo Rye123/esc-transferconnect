06ea26324d967215339ff4da25838f3c8bced9f4
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-18 15:21:07 +0800

    (WIP) feat: routing to membership page from programs list

--- bank/bank-client/src/routes/LoyaltyProgramMembershipPage.js
+++ bank/bank-client/src/routes/LoyaltyProgramMembershipPage.js
@@ -1,5 +1,5 @@
-import { useState, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useContext, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import Utils from '../utils/utils';
 import loyaltyPrograms_service from '../services/loyaltyPrograms_service';
 
@@ -10,19 +10,33 @@ import { useUserAuth } from '../hooks/UserAuthContext';
  * LoyaltyProgramMembershipPage - Form to create or modify an existing membership
  */
 const LoyaltyProgramMembershipPage = () => {
-    const [loyaltyProgram, setLoyaltyProgram] = useState({});
+    const [loyaltyProgram, setLoyaltyProgram] = useState(undefined);
+    const [searchParams, setSearchParams] = useSearchParams();
 
     // Load User
-    const params = useParams();
-    const loyaltyProgramId = params.loyaltyProgramId;
+    const loyaltyProgramId = searchParams.get("loyaltyProgramId") || "";
+    
+    useEffect(() => {
+        loyaltyPrograms_service.programs_getProgramById(loyaltyProgramId)
+        .then(loyaltyProgram => {
+            setLoyaltyProgram(loyaltyProgram);
+        })
+        .catch(err => {
+            console.error("LoyaltyProgramMembershipPage Error:", err);
+            setLoyaltyProgram({});
+        });
+    }, [])
 
     
     // Return HTML
+    if (typeof loyaltyProgram === 'undefined')
+        return (<h1>Loading membership page...</h1>);
     if (Utils.isEmptyObject(loyaltyProgram))
-        return (<h1>Loading...</h1>);
+        return (<h1>No such Loyalty Program</h1>); //TODO: decide where to redirect
+    
     return (
         <>
-            <h1>Set/Modify Existing Membership</h1>
+            <h1>Membership for {loyaltyProgram.loyaltyProgramName}</h1>
         </>
     )
 }
