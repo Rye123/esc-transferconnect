50782de472892cd5c225e4cb87d2d904ac30d804
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-19 01:07:41 +0800

    (WIP) feat: status message for transfers

--- bank/bank-client/src/classes/Transfer.js
+++ bank/bank-client/src/classes/Transfer.js
@@ -8,14 +8,16 @@ class Transfer {
      * @param {string} loyaltyProgramId 
      * @param {string} loyaltyProgramMembershipId 
      * @param {string} status 
+     * @param {string} statusMessage
      * @param {Date} submissionDate
      * @param {number} points
      */
-    constructor(transferId, loyaltyProgramId, loyaltyProgramMembershipId, status, submissionDate, points) {
+    constructor(transferId, loyaltyProgramId, loyaltyProgramMembershipId, status, statusMessage, submissionDate, points) {
         this.transferId = transferId;
         this.loyaltyProgramId = loyaltyProgramId;
         this.loyaltyProgramMembershipId = loyaltyProgramMembershipId;
         this.status = status;
+        this.statusMessage = statusMessage || "";
         this.submissionDate = submissionDate;
         this.points = points;
     }
