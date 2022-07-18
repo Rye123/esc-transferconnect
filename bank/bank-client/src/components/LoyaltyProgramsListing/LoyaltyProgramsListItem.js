c575acb1db43f0305f19f040011ab8a64d22ac47
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-19 01:24:02 +0800

    fix: exchange rate usage

--- bank/bank-client/src/components/LoyaltyProgramsListing/LoyaltyProgramsListItem.js
+++ bank/bank-client/src/components/LoyaltyProgramsListing/LoyaltyProgramsListItem.js
@@ -18,9 +18,9 @@ const LoyaltyProgramsListingElem = ({ loyaltyProgram }) => {
             <div className="vi_right">
                 <p className="title">{loyaltyProgram.loyaltyProgramName}</p>
                 <div className="content">
-                    <p>Ratio: {loyaltyProgram.exchangeRate}</p>
+                    <p><b>1</b> point = <b>{1/loyaltyProgram.exchangeRate}</b> {loyaltyProgram.loyaltyProgramName} points</p>
                     <p>Estimated Transfer Time: {loyaltyProgram.estTransferTime}</p>
-                    <p>Minimum Transfer Account: {loyaltyProgram.minTransfer}</p>
+                    <p>Minimum Transfer Amount: {loyaltyProgram.minTransfer} points</p> {/* check if using points or miles*/}
                 </div>
                 <Link to={{pathname: `/transfers/make_transfer`, search: `?loyaltyProgramId=${loyaltyProgram.loyaltyProgramId}`}} className="btn">Transfer Points</Link>
             </div>
