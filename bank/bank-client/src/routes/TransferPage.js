c15ab495790f3f62ef2d1176d365427e2545e4e5
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-18 17:37:01 +0800

    (WIP) feat: transfer page load transfer

--- bank/bank-client/src/routes/TransferPage.js
+++ bank/bank-client/src/routes/TransferPage.js
@@ -1,15 +1,36 @@
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import transfers_service from '../services/transfers_service';
+import Utils from '../utils/utils';
 
 /**
  * TransferPage - Shows transfer information for a single transfer.
  */
 const TransferPage = () => {
     // TODO: resolve loyaltyProgramId into loyaltyProgram
+    const [transfer, setTransfer] = useState(undefined);
     const [searchParams, setSearchParams] = useSearchParams();
     const transferId = searchParams.get("transferId") || "";
+
+    useEffect(() => {
+        transfers_service.transfer_getTransferById(transferId)
+        .then(transfer => {
+            setTransfer(transfer);
+        })
+        .catch(err => {
+            console.error("TransferPage Error:", err);
+            setTransfer({});
+        })
+    }, [])
     
+    if (typeof transfer === 'undefined')
+        return (<h1>Loading...</h1>);
+    if (Utils.isEmptyObject(transfer))
+        return (<h1>No such transfer</h1>); //TODO: decide where to route this
     return (
-        <h1>Transfer ID: {transferId}</h1>
+        <>
+            <h1>{transfer.transferId}</h1>
+        </>
     )
 }
 
