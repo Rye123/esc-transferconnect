484eacf7aa4c972d20044572c0f41fb452b66c70
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-18 21:34:02 +0800

    update: Navbar styling and Transfers title

--- bank/bank-client/src/components/Navbar.js
+++ bank/bank-client/src/components/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = () => {
             <a href='/' className='site-title'>Digibank</a>
             <NavbarLink to='/profile' content='Profile' />
             <NavbarLink to='/loyalty_programs' content='Loyalty Programs' />
-            <NavbarLink to='/transfers' content='Transfer History' />
+            <NavbarLink to='/transfers' content='Transfers' />
             <NavbarLink to='/logout' content='Logout' />
         </nav>
     )
