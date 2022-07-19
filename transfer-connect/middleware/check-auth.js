a4ce6581ba6387f2185acc099c54d6ea7a122030
wangsiyang68 <46260857+wangsiyang68@users.noreply.github.com>
2022-07-19 10:47:20 +0800

    feat: check the bankName with MongoDB documents

--- transfer-connect/middleware/check-auth.js
+++ transfer-connect/middleware/check-auth.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
-
-const HttpError = require('../models/http-error');
-
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+/* User Defined functions*/
+const HttpError = require('../models/http-error');
+const Bank = require("../models/bank");
+
 dotenv.config();
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
     
@@ -13,11 +16,20 @@ module.exports = (req, res, next) => {
       throw new Error('Authentication failed!');
     }
     const decodedToken = jwt.verify(token, process.env.JWT_TOKEN_SECRET_TC);
-    req.userData = { userId: decodedToken.userId };
+    req.userData = { bankName: decodedToken.bankName };
+    
+    // need to verify userId with bank details in mongoDB
+    let bank;
+    bank = await Bank.find(req.userData);
+    
+    if (!bank){
+      throw new Error('Authentication failed!');
+    }
+
     next();
   } catch (err) {
     const error = new HttpError('Authentication failed!', 401);
     return next(error);
   }
-  // need to verify userId with bank details in mongoDB
+  
 };
--- transfer-connect/tests/generate-token.js
+++ transfer-connect/tests/generate-token.js
@@ -4,11 +4,11 @@ const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
-function generateAccessToken(userId) {
-    return jwt.sign(userId, process.env.JWT_TOKEN_SECRET_TC, { expiresIn: '1800s' });
+function generateAccessToken(bankName) {
+    return jwt.sign(bankName, process.env.JWT_TOKEN_SECRET_TC, { expiresIn: '1800s' });
   }
   
 // console.log(process.env.SFTP_PASSWORD);
-console.log(generateAccessToken({ userId: 'DBS' }))
+console.log(generateAccessToken({ bankName: 'POSB' }))
 
 // console.log(require('crypto').randomBytes(64).toString('hex'))
