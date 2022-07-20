6b601479e6c9c3cd03ce6ff0456ba3d990c3d99b
wangsiyang68 <46260857+wangsiyang68@users.noreply.github.com>
2022-07-20 10:43:52 +0800

    tests: update tests to include authentication and documentation

--- transfer-connect/tests/tc_tests.js
+++ transfer-connect/tests/tc_tests.js
@@ -1,9 +1,12 @@
 var newman = require('newman'); // require Newman in your project
+const path = require('path');
+let envPath = path.resolve(__dirname, 'tc_test.postman_environment.json');
 
 // call newman.run to pass `options` object and wait for callback
 newman.run({
     collection: require('./TransferConnect.postman_collection.json'),
-    reporters: 'cli'
+    reporters: 'cli',
+    environment: envPath
 }, function (err) {
     if (err) { throw err; }
     console.log('Collection run complete!');
