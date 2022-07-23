ed41db641887b1d21de46fd714e00d8cd200b815
wangsiyang68 <46260857+wangsiyang68@users.noreply.github.com>
2022-07-23 17:56:12 +0800

    chore: remove the time limit for bank token

--- transfer-connect/tests/generate-token.js
+++ transfer-connect/tests/generate-token.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 function generateAccessToken(bankName) {
-    return jwt.sign(bankName, process.env.JWT_TOKEN_SECRET_TC, { expiresIn: '1800s' });
+    return jwt.sign(bankName, process.env.JWT_TOKEN_SECRET_TC);
   }
   
 // console.log(process.env.SFTP_PASSWORD);
