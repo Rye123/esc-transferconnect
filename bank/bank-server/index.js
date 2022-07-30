2dd04c9bc3f3f1d7bd237c5202586797777921d0
Ryan Toh <Rye123@users.noreply.github.com>
2022-07-30 17:25:31 +0800

    fix: account for additional errors

--- bank/bank-server/index.js
+++ bank/bank-server/index.js
@@ -132,6 +132,10 @@ app.use((error, request, response, next) => {
         case "TransferConnectError":
             console.log(`TransferConnect Error: ${error}`);
             return;
+        case "InvalidTransferError":
+            return response.status(error.status).json({ error: "invalid transfer status."} );
+        case "ExternalAuthenticationError":
+            return response.status(error.status).json({ error: "invalid authentication token."});
         default:
             console.log(error);
             return response.status(500).json({ error: "internal server error" })
