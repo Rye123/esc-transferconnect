24aaf75896087116c8a27183f803f62481ecf051
wangsiyang68 <46260857+wangsiyang68@users.noreply.github.com>
2022-08-08 18:09:42 +0800

    fix: validate createdTransfer fields

--- transfer-connect/models/transfer.js
+++ transfer-connect/models/transfer.js
@@ -2,15 +2,40 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const numericValidator = input => {
+    return /^(\d*)$/.test(input);
+}
+
+const alphaNumericValidator = input => {
+    return /^[A-Za-z0-9]+$/.test(input);
+}
+
 const transferSchema = new Schema({
-    memberId : { type: String, required: true },
-    memberFirstName : { type: String, required: true },
-    memberLastName : { type: String, required: true },
+    memberId : { type: String, validate: {
+        validator: function(v) {
+            if (this.loyaltyProgram === "GOPOINTS"){
+                return /^(\d{10}|\d{16})$/.test(v);
+            } else if (this.loyaltyProgram === "INDOMILES"){
+                return /^(\d{10})$/.test(v);
+            } else if (this.loyaltyProgram === "EMINENTGUEST") {
+                return /^(\d{12})$/.test(v)
+            } else if (this.loyaltyProgram === "CONRADCLUB") {
+                return /^(\d{9})$/.test(v)
+            } else if (this.loyaltyProgram === "MILLENIUMREWARDS") {
+                return /^(\d{10}[A-Za-z]{1})$/.test(v)
+            } else {
+                return false;
+            }
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }, required: true },
+    memberFirstName : { type: String, validate: alphaNumericValidator, required: true },
+    memberLastName : { type: String, validate: alphaNumericValidator, required: true },
     transferDate : { type: String, required: true },
-    amount : { type: String, required: true },
-    referenceNumber : { type: String, required: true },
-    partnerCode : { type: String, required: true }, 
-    loyaltyProgram: { type: String, required: true },  
+    amount : { type: String, validate: numericValidator,  required: true },
+    referenceNumber : { type: String, validate: alphaNumericValidator, required: true },
+    partnerCode : { type: String, validate: alphaNumericValidator, required: true }, 
+    loyaltyProgram: { type: String, validate: alphaNumericValidator, required: true },  
     status : { type: String, required: true},
     outcomeDetails : { type: String, required: true},
     sentToSFTP: { type: String, required: true}
