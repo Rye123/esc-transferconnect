669353b7d3f2715d5cbe7aeee1a91143cda8e066
wangsiyang68 <46260857+wangsiyang68@users.noreply.github.com>
2022-08-08 18:10:57 +0800

    fix: validate getTransferByRef post fields

--- transfer-connect/routes/bank-routes.js
+++ transfer-connect/routes/bank-routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require("express-validator");
 
 const bankControllers = require("../controllers/bank-controllers");
 
 /* Route for bank to request for transfer information */
 // router.get("/get-transfer/:tid",bankControllers.getTransferById);
-router.post("/accrual-update",bankControllers.getTransferByRef);
+router.post("/accrual-update",
+    [
+    check("referenceNumber").isAlphanumeric(),
+    check("partnerCode").isAlphanumeric()
+    ],
+    bankControllers.getTransferByRef);
 
 /* Route for bank to send transfer data */
 router.post("/accrual-req",bankControllers.createTransfer);
